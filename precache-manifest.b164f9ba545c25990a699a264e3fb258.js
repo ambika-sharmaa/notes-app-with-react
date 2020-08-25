1864a447525be3f1333b1c34dff8c1da5b6b255a
Ambika Sharma <ambika.sharma3009@gmai.com>
2020-08-25 23:32:17 +0530

    Updates

---
+++ precache-manifest.b164f9ba545c25990a699a264e3fb258.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db827d5046088111fa43aa6dda138fe",
+    "url": "/notes-with-react/index.html"
+  },
+  {
+    "revision": "bad964bdce397968b4c7",
+    "url": "/notes-with-react/static/js/2.ea1626bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/notes-with-react/static/js/2.ea1626bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c20d17db3c49f75944a",
+    "url": "/notes-with-react/static/js/main.f7d36e63.chunk.js"
+  },
+  {
+    "revision": "1e883c682eca55775e0b",
+    "url": "/notes-with-react/static/js/runtime-main.7128a8d2.js"
+  }
+]);
